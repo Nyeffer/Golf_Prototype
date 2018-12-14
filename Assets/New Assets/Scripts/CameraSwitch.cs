e8574714cba23424e2a74c76eb8d9e55ead409b3
Jeffherson Vega <jeffhersonlim@gmail.com>
2018-12-13 20:11:22 -0500

    Fix UI BirdEye

--- Assets/New Assets/Scripts/CameraSwitch.cs
+++ Assets/New Assets/Scripts/CameraSwitch.cs
@@ -7,13 +7,17 @@ public class CameraSwitch : MonoBehaviour {
 	// Public Variables
 	public Camera birdCam;
 	public Camera player;
+	public GameObject Normal;
+	public GameObject BirdsEye;
 
 	// Private Variables
 	private bool isBirds = false;
 
 	void Update() {
 		birdCam.gameObject.SetActive(isBirds);
+		BirdsEye.SetActive(isBirds);
 		player.gameObject.SetActive(!isBirds);
+		Normal.SetActive(!isBirds);
 	}
 
 	public void ChangeCam() {
